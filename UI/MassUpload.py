6330f101ba17b53c2329cb876876726bd307cc20
rluzuriaga <rodrigoluzuriaga@icloud.com>
2020-09-30 15:51:58 -0700

    Add class variables for grid

--- UI/MassUpload.py
+++ UI/MassUpload.py
@@ -7,6 +7,38 @@ from Database import Database
 from AWS import AWS
 
 class MassUpload(ttk.Frame):
+
+    # Grids
+    # Row 0
+    HEADER_LABEL_GRID = {'row': 0, 'column': 0, 'columnspan': 5, 'pady': 10, 'padx': 20}
+
+    # Row 1
+    UPDATE_LABEL_GRID = {'row': 1, 'column': 0, 'columnspan': 5, 'pady': 10, 'padx': 20}
+
+    # Row 2
+    PATH_TO_MASS_UPLOAD_LABEL_GRID = {'row': 2, 'column': 0 ,'columnspan': 5, 'padx': 20, 'pady': (10, 5)}
+
+    # Row 3
+    MASS_UPLOAD_PATH_INPUT_FIELD_GRID = {'row': 3, 'column':0, 'columnspan': 4, 'padx': (50, 5), 'ipady': 4}
+    MASS_UPLOAD_PATH_BUTTON_GRID = {'row': 3, 'column': 4, 'padx': (5, 50), 'ipady': 2}
+
+    # Row 4
+    S3_BUCKET_LOCATION_LABEL_GRID = {'row': 4, 'column': 0, 'columnspan': 5, 'padx': 20, 'pady': (25, 5)}
+
+    # Row 5
+    S3_BUCKET_SELECTOR_GRID = {'row': 5, 'column': 0, 'columnspan': 4, 'padx': (50, 5)}
+    REFRESH_S3_BUCKETS_BUTTON_GRID = {'row': 5, 'column': 4, 'padx': (8, 50), 'ipady': 3}
+
+    # Row 6
+    RADIO_BUTTON_LABEL_GRID = {'row': 6, 'column': 0, 'columnspan': 5, 'padx': 20, 'pady': (25, 5)}
+
+    # Row 7
+    RADIO_BUTTON_ALL_GRID = {'row': 7, 'column': 0, 'padx': 1, 'sticky': 'e'}
+    RADIO_BUTTON_VIDEO_GRID = {'row': 7, 'column': 1, 'padx': 1}
+
+    # Row 8
+    START_UPLOAD_BUTTON_GRID = {'row': 8, 'column': 0, 'columnspan': 2, 'pady': (20,15)}
+
     def __init__(self, parent, controller):
         ttk.Frame.__init__(self, parent, width=100, height=300, relief=tk.RIDGE)
         self.controller = controller
@@ -33,7 +65,7 @@ class MassUpload(ttk.Frame):
             font=('Helvetica', 18, 'underline'),
             justify=tk.CENTER
         )
-        self.header_label.grid(row=0, column=0, columnspan=5, pady=10, padx=20)
+        self.header_label.grid(self.HEADER_LABEL_GRID)
 
         # Row 1
         self.update_label = ttk.Label(
@@ -42,7 +74,7 @@ class MassUpload(ttk.Frame):
             font=('Helvetica', 15),
             justify=tk.CENTER
         )
-        self.update_label.grid(row=1, column=0, columnspan=5, pady=10, padx=20)
+        self.update_label.grid(self.UPDATE_LABEL_GRID)
 
         # Row 2
         self.path_to_mass_upload_label = ttk.Label(
@@ -51,7 +83,7 @@ class MassUpload(ttk.Frame):
             font=('Helvetica', 15),
             justify=tk.CENTER
         )
-        self.path_to_mass_upload_label.grid(row=2, column=0 ,columnspan=5, padx=20, pady=(10, 5))
+        self.path_to_mass_upload_label.grid(self.PATH_TO_MASS_UPLOAD_LABEL_GRID)
 
         # Row 3
         self.mass_upload_path = tk.StringVar()
@@ -60,14 +92,14 @@ class MassUpload(ttk.Frame):
             width=35,
             textvariable=self.mass_upload_path
         )
-        self.mass_upload_path_input_field.grid(row=3, column=0, columnspan=4, padx=(50, 5), ipady=4)
+        self.mass_upload_path_input_field.grid(self.MASS_UPLOAD_PATH_INPUT_FIELD_GRID)
 
         self.mass_upload_path_button = ttk.Button(
             self,
             image=self.open_folder_icon_path,
             command=self.open_folder_path
         )
-        self.mass_upload_path_button.grid(row=3, column=4, padx=(5, 50), ipady=2)
+        self.mass_upload_path_button.grid(self.MASS_UPLOAD_PATH_BUTTON_GRID)
 
         # Row 4
         self.s3_bucket_location_label = ttk.Label(
@@ -76,7 +108,7 @@ class MassUpload(ttk.Frame):
             font=('Helvetica', 15),
             justify=tk.CENTER
         )
-        self.s3_bucket_location_label.grid(row=4, column=0, columnspan=5, padx=20, pady=(25, 5))
+        self.s3_bucket_location_label.grid(self.S3_BUCKET_LOCATION_LABEL_GRID)
         
         # Row 5
         self.s3_bucket_name = tk.StringVar()
@@ -87,14 +119,14 @@ class MassUpload(ttk.Frame):
             width=35,
             state='readonly'
         )
-        self.s3_bucket_selector.grid(row=5, column=0, columnspan=4, padx=(50, 5))
+        self.s3_bucket_selector.grid(self.S3_BUCKET_SELECTOR_GRID)
 
         self.refresh_s3_buckets_button = ttk.Button(
             self,
             image=self.refresh_button_image,
             command=self.refresh_s3_buckets
         )
-        self.refresh_s3_buckets_button.grid(row=5, column=4, padx=(8, 50), ipady=3)
+        self.refresh_s3_buckets_button.grid(self.REFRESH_S3_BUCKETS_BUTTON_GRID)
 
         # Row 6
         self.radio_button_label = ttk.Label(
@@ -103,7 +135,7 @@ class MassUpload(ttk.Frame):
             font=('Helvetica', 15),
             justify=tk.CENTER
         )
-        self.radio_button_label.grid(row=6, column=0, columnspan=5, padx=20, pady=(25, 5))
+        self.radio_button_label.grid(self.RADIO_BUTTON_LABEL_GRID)
 
         # Row 7
         self.radio_button_var = tk.IntVar()
@@ -116,7 +148,7 @@ class MassUpload(ttk.Frame):
 
             command=lambda: print(self.radio_button_var.get())
         )
-        self.radio_button_all.grid(row=7, column=0, padx=1, sticky='e')
+        self.radio_button_all.grid(self.RADIO_BUTTON_ALL_GRID)
 
         self.radio_button_video = ttk.Radiobutton(
             self,
@@ -125,7 +157,7 @@ class MassUpload(ttk.Frame):
             variable=self.radio_button_var,
             command=lambda: print(self.radio_button_var.get())
         )
-        self.radio_button_video.grid(row=7, column=1, padx=1)
+        self.radio_button_video.grid(self.RADIO_BUTTON_VIDEO_GRID)
 
         # Row 8
         self.start_upload_button = ttk.Button(
@@ -134,7 +166,7 @@ class MassUpload(ttk.Frame):
             style='regular.TButton',
             command=self.start_upload
         )
-        self.start_upload_button.grid(row=8, column=0, columnspan=2, pady=(20,15))
+        self.start_upload_button.grid(self.START_UPLOAD_BUTTON_GRID)
 
     def open_folder_path(self):
         path = filedialog.askdirectory()

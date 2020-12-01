43220c988793a09d0407888b1a4f9b9f5d9eaf9b
rluzuriaga <rodrigoluzuriaga@icloud.com>
2020-12-01 13:24:12 -0800

    Add logging for ProgramController.py

--- S3_File_Uploader/UI/ProgramController.py
+++ S3_File_Uploader/UI/ProgramController.py
@@ -1,3 +1,4 @@
+import logging
 import tkinter as tk
 from tkinter import ttk
 
@@ -5,13 +6,18 @@ from .MainWindow import MainWindow
 from .SetupWindow import SetupWindow
 from .MassUpload import MassUpload
 
+# Setup logger
+logger = logging.getLogger('main_logger')
+
+
 class ProgramController(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        logger.debug("Initializing the Tkinter program controller.")
 
         # Make the application not resizable
         self.resizable(False, False)
-        
+
         self.container = ttk.PanedWindow(self, orient=tk.HORIZONTAL)
         self.container.pack(fill=tk.BOTH, expand=True)
 
@@ -20,6 +26,8 @@ class ProgramController(tk.Tk):
         for F in (MainWindow, SetupWindow, MassUpload):
             frame = F(self.container, self)
             self.frames[F] = frame
+
+            logger.debug(f"Adding {F.__name__} to the program controller frames.")
 
         # Open main page
         self.add_frame_to_paned_window(MainWindow)
@@ -35,37 +43,52 @@ class ProgramController(tk.Tk):
         positionDown = int((screen_y - 200)/2 - windowHeight/2)
 
         self.geometry(f"+{positionRight}+{positionDown}")
-    
+
+        logger.debug("Set location of main window on the screen.")
+
     def get_screen_size(self):
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
 
+        logger.debug("Returning screen size.")
+
         return screen_width, screen_height
-    
+
     def add_frame_to_paned_window(self, frame_):
         frame = self.frames[frame_]
         self.container.add(frame, weight=1)
-    
+
+        logger.debug(f'Adding "{frame_.__name__}" frame to paned window.')
+
     def remove_paned_window_frame(self, frame_from_active_panes):
         self.container.forget(frame_from_active_panes)
-    
+        logger.debug(f'Removing "{str(frame_from_active_panes)}" frame from paned window.')
+
     def active_panes(self):
         return self.container.panes()
-    
+
     def get_last_frame(self):
         last_frame = list(self.frames.keys())[-1]
         return last_frame
-    
+
     def enable_main_window_buttons(self):
         mw = self.frames[MainWindow]
         mw.enable_all_buttons()
-    
+
+        logger.debug('Enabling all "MainWindow" buttons.')
+
     def disable_main_window_buttons(self):
         mw = self.frames[MainWindow]
         mw.disable_all_buttons()
 
+        logger.debug('Disabling all "MainWindow" buttons.')
+
     def setup_start_after(self):
         self.frames[SetupWindow].start_after()
-    
+
+        logger.debug('Starting the tkinter after method for "SetupWindow".')
+
     def setup_stop_after(self):
         self.frames[SetupWindow].stop_after()
+
+        logger.debug('Stopping the tkinter after method for "SetupWindow".')

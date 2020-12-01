9c958590cee2d23df9e7f76dd880b571569b7c20
rluzuriaga <rodrigoluzuriaga@icloud.com>
2020-12-01 12:01:49 -0800

    Add logging for main.py

--- S3_File_Uploader/main.py
+++ S3_File_Uploader/main.py
@@ -1,9 +1,31 @@
+import os
+import logging
+from datetime import datetime
+
 from UI.ProgramController import ProgramController
 
 
+# Set up file based logger
+current_datetime = datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+log_path = os.getcwd() + f'/logs/{current_datetime}.log'
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(filename)s:%(funcName)s - %(message)s',
+                    filename=log_path,
+                    filemode='a')
+
+logger = logging.getLogger('main_logger')
+
+logger.debug("Logger setup.")
+
+
 def main():
+    logger.debug("Starting program.")
+
     pc = ProgramController()
     pc.mainloop()
+
+    logger.debug("Exiting program.")
 
 
 if __name__ == "__main__":

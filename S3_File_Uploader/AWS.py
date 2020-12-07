1a6ae6ccc4920212960b45037cbe69b2fa6db6ac
rluzuriaga <rodrigoluzuriaga@icloud.com>
2020-12-07 14:57:40 -0800

    Add logging for AWS.py

--- S3_File_Uploader/AWS.py
+++ S3_File_Uploader/AWS.py
@@ -1,15 +1,21 @@
+import logging
+
 import boto3
 import botocore
 
 from Database import Database
 
+logger = logging.getLogger('main_logger')
+
 
 class AWS:
-    def __init__(self):
-        pass
+    # def __init__(self):
+    #     pass
 
     @staticmethod
     def test_connection(aws_access_key_id, aws_secret_access_key, region_code):
+        logger.debug(f'Testing AWS connection.')
+
         try:
             client = boto3.client(
                 'sts',
@@ -23,9 +29,15 @@ class AWS:
             return response
         except botocore.exceptions.ClientError as err:
             if 'SignatureDoesNotMatch' in str(err) or 'InvalidClientTokenId' in str(err):
+                logger.warning(f'Raising AWSKeyException.')
+
                 raise AWSKeyException
         except botocore.exceptions.EndpointConnectionError:
+            logger.warning(f'Raising NoConnectionError')
+
             raise NoConnectionError
+
+        logger.debug(f'Connection successful.')
 
     @staticmethod
     def get_s3_buckets():
@@ -42,11 +54,11 @@ class AWS:
 
                 buckets_dict = client.list_buckets()
 
-                buckets_values = tuple(
-                    bucket_name['Name'] for bucket_name in buckets_dict['Buckets'])
+                buckets_values = tuple(bucket_name['Name'] for bucket_name in buckets_dict['Buckets'])
+
+                logger.debug(f'Retrieving all S3 buckets.')
+
                 return buckets_values
-                # except Exception as e:
-                #     pass
 
     @staticmethod
     def upload_file(file_path, bucket_name, file_name, ProgressCallback=None):
@@ -59,6 +71,8 @@ class AWS:
                 aws_secret_access_key=aws_config[1],
                 region_name=aws_config[2]
             )
+
+            logger.debug(f'Uploading file to S3.')
 
             client.upload_file(file_path, bucket_name, file_name,
                                Callback=ProgressCallback)
@@ -75,6 +89,8 @@ class AWS:
                 region_name=aws_config[2]
             )
 
+            logger.debug(f'Creating folder in S3 Bucket.')
+
             client.put_object(Bucket=bucket_name, Key=(folder_name+'/'))
 
     def create_multiple_folders_in_bucket(self, bucket_name, list_of_folder_names):
@@ -89,6 +105,8 @@ class AWS:
             )
 
             bucket_objects_dict = self.get_bucket_objects_as_dict(bucket_name)
+
+            logger.debug(f'Creating multiple folders in the S3 Bucket.')
 
             for folder_name in list_of_folder_names:
                 if folder_name + '/' not in bucket_objects_dict:
@@ -109,6 +127,8 @@ class AWS:
             )
 
             objects = client.list_objects_v2(Bucket=bucket_name)
+
+            logger.debug(f'Retrieving S3 Bucket objects as a dictionary.')
 
             try:
                 return {item['Key']: item['Size'] for item in objects['Contents']}

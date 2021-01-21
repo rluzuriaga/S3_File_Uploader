d8151cb6096ee5f68f7be74f4aa84f39129efbf8
rluzuriaga <rodrigoluzuriaga@icloud.com>
2021-01-21 15:23:02 -0800

    Add code to make the setup work on mac
    Could not use cx_Freeze to create the mac app, using py2app instead.
    The script now creates an app bundle and dmg when using a Mac, and a zip folder and msi when using Windows.
    
    #25

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import os
 import sys
+import shutil
 import zipfile
 import platform
 import cx_Freeze
+import subprocess
 import distutils.core
-from shutil import rmtree, copy
 from setuptools import find_packages
 
 from S3_File_Uploader import APP_TITLE, APP_VERSION
@@ -13,6 +14,8 @@ from S3_File_Uploader import APP_TITLE, APP_VERSION
 WINDOWS = sys.platform.startswith('win32')
 MAC = sys.platform.startswith('darwin')
 
+DMG_NAME = f"{APP_TITLE} Installer"
+
 BUILD_DIR = os.path.join(os.getcwd(), 'build')
 DIST_DIR = os.path.join(os.getcwd(), 'dist')
 LOGS_DIR = os.path.join(os.getcwd(), 'logs')
@@ -20,22 +23,30 @@ FINAL_DISTRIBUTION_DIR = os.path.join(os.getcwd(), 'distribution')
 
 DIST_NAME = f'{APP_TITLE.replace(" ", "_")}-{APP_VERSION}-{platform.system().lower()}_{platform.machine().lower()}'
 DIST_PATH = os.path.join(BUILD_DIR, DIST_NAME)
-EXEC_ICON = os.path.join(os.getcwd(), 'S3_File_Uploader', 'UI', 'images', 'logo.ico')
-EXEC_BASE = None
+
+ICON = None
+BASE = None
 EXEC_NAME = None
 UPGRADE_CODE = None
 PRODUCT_CODE = None
 
+COMMANDS = dict()
+
+
 if WINDOWS:
     import msilib
+    from cx_Freeze import setup
 
-    EXEC_BASE = 'Win32GUI'
+    BASE = 'Win32GUI'
     EXEC_NAME = f'{APP_TITLE}.exe'
     UPGRADE_CODE = '{6ADD4904-6A36-4D2F-BF96-8241B3D8D474}'
     PRODUCT_CODE = msilib.gen_uuid()
+    ICON = os.path.join(os.getcwd(), 'S3_File_Uploader', 'UI', 'images', 'logo.ico')
 
 if MAC:
-    EXEC_NAME = APP_TITLE
+    from setuptools import setup
+
+    ICON = os.path.join(os.getcwd(), 'S3_File_Uploader', 'UI', 'images', 'icon.icns')
 
 
 class Clean(distutils.core.Command):
@@ -66,115 +77,310 @@ class Clean(distutils.core.Command):
         Command execution.
         """
         if os.path.exists(BUILD_DIR):
-            rmtree(BUILD_DIR)
+            shutil.rmtree(BUILD_DIR)
 
         if os.path.exists(DIST_DIR):
-            rmtree(DIST_DIR)
+            shutil.rmtree(DIST_DIR)
 
         for dir in self.pycache_directories:
             if os.path.exists(dir):
-                rmtree(dir)
+                shutil.rmtree(dir)
 
         if self.full_clean:
             if os.path.exists(FINAL_DISTRIBUTION_DIR):
-                rmtree(FINAL_DISTRIBUTION_DIR)
+                shutil.rmtree(FINAL_DISTRIBUTION_DIR)
 
             if os.path.exists(LOGS_DIR):
-                rmtree(LOGS_DIR)
+                shutil.rmtree(LOGS_DIR)
 
             if os.path.exists(os.path.join(os.getcwd(), 'db.sqlite3')):
                 os.remove(os.path.join(os.getcwd(), 'db.sqlite3'))
 
 
-class BuildMsi(cx_Freeze.bdist_msi):
-    def initialize_options(self):
-        super().initialize_options()
-        self.target_name = DIST_NAME + '.msi'
+COMMANDS['clean'] = Clean
+
+
+if WINDOWS:
+    class Build(cx_Freeze.build_exe):
+        def initialize_options(self):
+            self.dist_dir = None
+            user_options = cx_Freeze.build_exe.user_options
+            user_options.extend([('dist-dir=', 'd', "directory where to put final distributions in [default: dist]")])
+            super().initialize_options()
+
+            if not os.path.exists(FINAL_DISTRIBUTION_DIR):
+                os.mkdir(FINAL_DISTRIBUTION_DIR)
+
+        def finalize_options(self):
+            if self.dist_dir is None:
+                self.dist_dir = self.build_exe
+            super().finalize_options()
+
+        def run(self):
+            super().run()
+            self.execute(self.make_dist_folder, ())
+            self.execute(self.make_zip, ())
+            self.execute(self.copy_to_final_distribute_folder, ())
+
+        def make_dist_folder(self):
+            """ Make dist folder to add distribution files to. """
+            if not os.path.exists(DIST_DIR):
+                os.mkdir(DIST_DIR)
+
+        def make_zip(self):
+            """ Create ZIP distribution. """
+            self.zip_path = os.path.join(DIST_DIR, f'{DIST_NAME}.zip')
+            with zipfile.ZipFile(self.zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                for dirpath, dirnames, filenames in os.walk(os.path.join(os.getcwd(), self.build_exe)):
+                    for file in filenames:
+                        path = os.path.join(os.getcwd(), dirpath, file)
+                        arcname = os.path.relpath(path, self.build_exe)
+                        zipf.write(path, arcname)
+
+        def copy_to_final_distribute_folder(self):
+            if os.path.exists(self.zip_path):
+                shutil.copy(self.zip_path, FINAL_DISTRIBUTION_DIR)
+
+    COMMANDS['build'] = Build
+
+if MAC:
+    class Create(distutils.core.Command):
+        user_options = []
+
+        def initialize_options(self) -> None:
+            pass
+
+        def finalize_options(self) -> None:
+            pass
+
+        def run(self) -> None:
+            self.run_command('py2app')
+            self.execute(self.create_application_folder_shortcut, ())
+            self.execute(self.create_dmg, ())
+            self.execute(self.remove_application_folder_shortcut, ())
+
+        def create_application_folder_shortcut(self) -> None:
+            out_code = subprocess.run(['ln', '-s', '/Applications', f'{DIST_DIR}'])
+
+            if out_code.returncode == 0:
+                print("Applications folder shortcut created.")
+            else:
+                print("ERROR: Was not able to create Applications folder shortcut")
+
+        def create_dmg(self) -> None:
+            out_code = subprocess.run(
+                ['hdiutil', 'create', f'{DIST_DIR}/{DMG_NAME}.dmg',
+                 '-ov', '-volname', f'"{DMG_NAME}"', '-fs', 'HFS+', '-srcfolder',
+                 f'{DIST_DIR}/']
+            )
+
+            if out_code.returncode != 0:
+                print("ERROR: Was not able to create dmg.")
+
+        def remove_application_folder_shortcut(self) -> None:
+            os.remove(os.path.join(DIST_DIR, 'Applications'))
+
+    COMMANDS['create'] = Create
+
+if WINDOWS:
+    class Create(distutils.core.Command):
+        user_options = []
+
+        def initialize_options(self) -> None:
+            pass
+
+        def finalize_options(self) -> None:
+            pass
+
+        def run(self) -> None:
+            self.run_command('bdist_msi')
+
+    class BuildExe(cx_Freeze.build_exe):
+        def initialize_options(self) -> None:
+            self.dist_dir = None
+            super().initialize_options()
+
+        def finalize_options(self) -> None:
+            if self.dist_dir is None:
+                self.dist_dir = self.build_exe
+            super().finalize_options()
+
+        def run(self) -> None:
+            super().run()
+            self.execute(self.make_dist_folder, ())
+            self.execute(self.make_zip, ())
+
+        def make_dist_folder(self) -> None:
+            """ Make dist folder to add distribution files to. """
+            if not os.path.exists(DIST_DIR):
+                os.mkdir(DIST_DIR)
+
+        def make_zip(self) -> None:
+            """ Create ZIP distribution. """
+            self.zip_path = os.path.join(DIST_DIR, f'{DIST_NAME}.zip')
+            with zipfile.ZipFile(self.zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                for dirpath, dirnames, filenames in os.walk(os.path.join(os.getcwd(), self.build_exe)):
+                    for file in filenames:
+                        path = os.path.join(os.getcwd(), dirpath, file)
+                        arcname = os.path.relpath(path, self.build_exe)
+                        zipf.write(path, arcname)
+
+    class BuildMsi(cx_Freeze.bdist_msi):
+        def initialize_options(self) -> None:
+            super().initialize_options()
+            self.target_name = DIST_NAME + '.msi'
+
+        def finalize_options(self) -> None:
+            super().finalize_options()
+
+        def run(self) -> None:
+            super().run()
+
+    COMMANDS['build_exe'] = BuildExe
+    COMMANDS['bdist_msi'] = BuildMsi
+    COMMANDS['create'] = Create
+
+
+class Distribute(distutils.core.Command):
+    user_options = []
+
+    def initialize_options(self) -> None:
 
         if not os.path.exists(FINAL_DISTRIBUTION_DIR):
             os.mkdir(FINAL_DISTRIBUTION_DIR)
 
-    def finalize_options(self):
-        super().finalize_options()
+        if MAC:
+            self.dist_package_path = [
+                os.path.join(DIST_DIR, APP_TITLE + '.app'),
+                os.path.join(DIST_DIR, DMG_NAME + '.dmg')
+            ]
 
-    def run(self):
-        super().run()
-        self.execute(self.copy_to_final_distribute_folder, ())
+        if WINDOWS:
+            self.dist_package_path = [
+                os.path.join(DIST_DIR, DIST_NAME + '.msi'),
+                os.path.join(DIST_DIR, DIST_NAME + '.zip')
+            ]
 
-    def copy_to_final_distribute_folder(self):
-        if os.path.exists(os.path.join(DIST_DIR, DIST_NAME + '.msi')):
-            copy(os.path.join(DIST_DIR, DIST_NAME + '.msi'), FINAL_DISTRIBUTION_DIR)
+    def finalize_options(self) -> None:
+        pass
 
-
-class BuildExe(cx_Freeze.build_exe):
-    def initialize_options(self):
-        self.dist_dir = None
-        super().initialize_options()
-
-        if not os.path.exists(FINAL_DISTRIBUTION_DIR):
-            os.mkdir(FINAL_DISTRIBUTION_DIR)
-
-    def finalize_options(self):
-        if self.dist_dir is None:
-            self.dist_dir = self.build_exe
-        super().finalize_options()
-
-    def run(self):
-        super().run()
-        self.execute(self.make_dist_folder, ())
-        self.execute(self.make_zip, ())
-        self.execute(self.copy_to_final_distribute_folder, ())
-
-    def make_dist_folder(self):
-        """ Make dist folder to add distribution files to. """
-        if not os.path.exists(DIST_DIR):
-            os.mkdir(DIST_DIR)
-
-    def make_zip(self):
-        """ Create ZIP distribution. """
-        self.zip_path = os.path.join(DIST_DIR, f'{DIST_NAME}.zip')
-        with zipfile.ZipFile(self.zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-            for dirpath, dirnames, filenames in os.walk(os.path.join(os.getcwd(), self.build_exe)):
-                for file in filenames:
-                    path = os.path.join(os.getcwd(), dirpath, file)
-                    arcname = os.path.relpath(path, self.build_exe)
-                    zipf.write(path, arcname)
-
-    def copy_to_final_distribute_folder(self):
-        if os.path.exists(self.zip_path):
-            copy(self.zip_path, FINAL_DISTRIBUTION_DIR)
+    def run(self) -> None:
+        for path in self.dist_package_path:
+            try:
+                shutil.copy(path, FINAL_DISTRIBUTION_DIR)
+            except:
+                os.rmdir(FINAL_DISTRIBUTION_DIR)
+                shutil.copytree(path[:path.rfind('/')], FINAL_DISTRIBUTION_DIR)
 
 
-COMMANDS = {
-    'clean': Clean,
-    'bdist_msi': BuildMsi,
-    'build_exe': BuildExe
-}
-
-PACKAGES = [
-    'botocore',
-    'boto3',
-    'ffmpeg',
-    'pexpect',
-    'requests',
-    'html'
-]
-
-INCLUDES = [
-    'botocore',
-    'boto3',
-    'ffmpeg',
-    'pexpect',
-    'requests',
-    'html'
-]
-
-INCLUDE_FILES = [
-    ('S3_File_Uploader', 'S3_File_Uploader')
-]
+COMMANDS['distribute'] = Distribute
 
 
-cx_Freeze.setup(
+if WINDOWS:
+    extra_options = dict(
+        options={
+            'build_exe': {
+                'packages': [
+                    'botocore',
+                    'boto3',
+                    'ffmpeg',
+                    'pexpect',
+                    'requests',
+                    'html'
+                ],
+                'includes': [
+                    'botocore',
+                    'boto3',
+                    'ffmpeg',
+                    'pexpect',
+                    'requests',
+                    'html'
+                ],
+                'include_files': [
+                    ('S3_File_Uploader', 'S3_File_Uploader')
+                ]
+            },
+            'bdist_msi': {
+                'upgrade_code': UPGRADE_CODE,
+                'all_users': False,
+                'product_code': PRODUCT_CODE,
+
+                # https://stackoverflow.com/questions/15734703/use-cx-freeze-to-create-an-msi-that-adds-a-shortcut-to-the-desktop#15736406
+                'data': {
+                    'Shortcut': [
+                        (
+                            "DesktopShortcut",                  # Shortcut
+                            "DesktopFolder",                    # Directory_
+                            "S3 File Uploader",                 # Name
+                            "TARGETDIR",                        # Component_
+                            "[TARGETDIR]S3 File Uploader.exe",  # Target
+                            None,                               # Arguments
+                            None,                               # Description
+                            None,                               # Hotkey
+                            None,                               # Icon
+                            None,                               # IconIndex
+                            None,                               # ShowCmd
+                            "TARGETDIR"                         # WkDir
+                        ),
+                        (
+                            "MenuShortcut",                     # Shortcut
+                            "ProgramMenuFolder",                # Directory_
+                            "S3 File Uploader",                 # Name
+                            "TARGETDIR",                        # Component_
+                            "[TARGETDIR]S3 File Uploader.exe",  # Target
+                            None,                               # Arguments
+                            None,                               # Description
+                            None,                               # Hotkey
+                            None,                               # Icon
+                            None,                               # IconIndex
+                            None,                               # ShowCmd
+                            "TARGETDIR"                         # WkDir
+                        )
+                    ]
+                }
+            }
+        },
+        executables=[
+            cx_Freeze.Executable(
+                'S3_File_Uploader/main.py',
+                targetName='S3 File Uploader',
+                base=BASE,
+                icon=ICON
+            )
+        ]
+    )
+
+if MAC:
+    extra_options = dict(
+        app=['S3_File_Uploader/main.py'],
+        options={
+            'py2app': {
+                # Uncomment if needing
+                # 'graph': True,
+
+                'iconfile': ICON,
+                'emulate_shell_environment': True,
+                'argv_emulation': False,
+                'extension': '.app',
+                'site_packages': True,
+                'resources': [
+                    'S3_File_Uploader/UI/images'
+                ],
+                # Any package that is not part of the standard library
+                # that is used as an import, needs to be added here
+                'packages': [
+                    'botocore',
+                    'boto3',
+                    'ffmpeg',
+                    'pexpect',
+                    'requests',
+                ]
+            }
+        },
+        setup_requires=['py2app']
+    )
+
+setup(
     cmdclass=COMMANDS,
     name=APP_TITLE,
     version=APP_VERSION,
@@ -185,72 +391,26 @@ cx_Freeze.setup(
     maintainer='Rodrigo Luzuriaga',
     maintainer_email='me@rodrigoluzuriaga.com',
     packages=find_packages(),
+    package_data={
+        'S3_File_Uploader': ['UI/images/*.*']
+    },
+    install_requires=[
+        'botocore',
+        'boto3',
+        'ffmpeg-python',
+        'pexpect',
+        'requests',
+        'html' if WINDOWS else ''
+    ],
+    zip_safe=True,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: MacOS X',
-        'Environment :: Win32 (MS Windows)',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Operating System :: OS Independent',
+        'Operating System :: MacOS :: MacOS X',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: SQL',
-        'Topic :: Utilities'
+        'Programming Language :: SQL'
     ],
-
-    options={
-        'build_exe': {
-            'packages': PACKAGES,
-            'includes': INCLUDES,
-            'include_files': INCLUDE_FILES
-        },
-        'bdist_msi': {
-            'upgrade_code': UPGRADE_CODE,
-            'all_users': False,
-            'product_code': PRODUCT_CODE,
-
-            # https://stackoverflow.com/questions/15734703/use-cx-freeze-to-create-an-msi-that-adds-a-shortcut-to-the-desktop#15736406
-            'data': {
-                'Shortcut': [
-                    (
-                        "DesktopShortcut",                  # Shortcut
-                        "DesktopFolder",                    # Directory_
-                        "S3 File Uploader",                 # Name
-                        "TARGETDIR",                        # Component_
-                        "[TARGETDIR]S3 File Uploader.exe",  # Target
-                        None,                               # Arguments
-                        None,                               # Description
-                        None,                               # Hotkey
-                        None,                               # Icon
-                        None,                               # IconIndex
-                        None,                               # ShowCmd
-                        "TARGETDIR"                         # WkDir
-                    ),
-                    (
-                        "MenuShortcut",                     # Shortcut
-                        "ProgramMenuFolder",                # Directory_
-                        "S3 File Uploader",                 # Name
-                        "TARGETDIR",                        # Component_
-                        "[TARGETDIR]S3 File Uploader.exe",  # Target
-                        None,                               # Arguments
-                        None,                               # Description
-                        None,                               # Hotkey
-                        None,                               # Icon
-                        None,                               # IconIndex
-                        None,                               # ShowCmd
-                        "TARGETDIR"                         # WkDir
-                    )
-                ]
-            }
-        }
-    },
-
-    executables=[
-        cx_Freeze.Executable(
-            script='S3_File_Uploader/main.py',
-            targetName=EXEC_NAME,
-            base=EXEC_BASE,
-            icon=EXEC_ICON
-        )
-    ],
+    **extra_options
 )

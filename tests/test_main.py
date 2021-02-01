39c9cea170e869ac3b9cb1695d0c14de983bf4b8
Rodrigo Luzuriaga <rodrigoluzuriaga@icloud.com>
2021-02-01 15:35:05 -0800

    Add asserts to check that the mass upload button on the main window gets activated. When there are no settings saved the mass upload button on the main window should be disabled. When saving the settings the mass upload button should get enabled. #27

--- tests/test_main.py
+++ tests/test_main.py
@@ -76,15 +76,22 @@ class MainTestCase(unittest.TestCase):
         self.pc = ProgramController()
 
         self.pc.add_frame_to_paned_window(SetupWindow)
+        main_window = self.pc.select_frame(MainWindow)
         setup_window = self.pc.select_frame(SetupWindow)
 
-        # Interact with the UI
+        # Make sure that the mass upload button is disabled before any settings are saved
+        self.assertEqual(str(main_window.mass_upload_window_button.cget('state')), 'disabled')
+
+        # Interact with the UI to add the AWS keys, region name, and "click" the save button
         setup_window.access_key_id_string.set(os.environ.get('AWS_ACCESS_KEY_ID'))
         setup_window.secret_key_string.set(os.environ.get('AWS_SECRET_KEY'))
         setup_window.region_name_var.set(os.environ.get('AWS_REGION_NAME'))
         setup_window.save_button.invoke()
 
         self._update_program_controller_loop()
+
+        # Make sure that the mass upload button is now enabled after the save
+        self.assertEqual(str(main_window.mass_upload_window_button.cget('state')), 'normal')
 
         # Make sure that the settings saved message gets displayed.
         self.assertEqual(setup_window.setup_window_output_message_variable.get(), 'Settings saved.')

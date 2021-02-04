8b4683a9151e0b3b74ea20ec9d8b698e26e31edb
Rodrigo Luzuriaga <rodrigoluzuriaga@icloud.com>
2021-02-04 15:17:30 -0800

    Cleanup utils.py. - Make sure that the database gets deleted instead of throwing a PermisionError. - Make the `update_program_controller_loop` default to 0.1 seconds. - Remove `close_program()` and make `destroy_program()` do both. #27

--- tests/integration_tests/utils.py
+++ tests/integration_tests/utils.py
@@ -1,5 +1,6 @@
 import os
 import time
+import _tkinter
 
 from S3_File_Uploader import DatabasePath
 from S3_File_Uploader.UI.ProgramController import ProgramController
@@ -17,17 +18,24 @@ def remove_db_file() -> None:
     db_file_path = DatabasePath.get()
 
     if os.path.exists(db_file_path):
-        os.remove(db_file_path)
+        # Since the tests run faster than what Python can remove the file,
+        #  the function is now recursive to wait until the program actually ends
+        #  before removing the database file.
+        try:
+            os.remove(db_file_path)
+        except PermissionError:
+            time.sleep(1)
+            remove_db_file()
 
 
-def update_program_controller_loop(program_controller: ProgramController, seconds: int = 1) -> None:
+def update_program_controller_loop(program_controller: ProgramController, seconds: float = 0.1) -> None:
     """ Run a loop for the guiven seconds that updates idle tasks and user input tasks.
     This function needs to be used because to be able to test Tkinter UI elements , the test
     cannot use the mainloop function of tkinter. That means that Tkinter needs to be updated manually.
 
     Args:
-        seconds (int, optional): Seconds to run the loop. Defaults to 1.
-            Each second, the update and update_idletasks functions run around 2600 times (depending on CPU clock speed).
+        seconds (float, optional): Seconds to run the loop. Defaults to 0.1.
+            Each 0.1 seconds, the update and update_idletasks functions run around 880 times (depending on CPU clock speed).
     """
 
     secs = seconds
@@ -37,6 +45,9 @@ def update_program_controller_loop(program_controller: ProgramController, second
         elapsed_time = current_time - start_time
 
         if elapsed_time < secs:
+            # Since the tests are not running with the mainloop, then the program needs to be forced to update
+            # TODO: Test which method is best. They should all do the same thing but I get weird issues if I don't include all three
+            program_controller.dooneevent(_tkinter.ALL_EVENTS | _tkinter.DONT_WAIT)
             program_controller.update_idletasks()
             program_controller.update()
         else:
@@ -48,9 +59,6 @@ def open_program() -> None:
     return pc
 
 
-def close_program(pc: ProgramController) -> None:
-    pc.quit()
-
-
 def destroy_program(pc: ProgramController) -> None:
+    pc.quit()
     pc.destroy()

d87cdfe0991626ddcd5adafc2f937185eba38398
rluzuriaga <rodrigoluzuriaga@icloud.com>
2021-02-07 19:56:59 -0800

    Removing testing file and actions for this branch #27

--- testing_env_vars.py
+++
@@ -1,9 +0,0 @@
-import os
-
-a = os.environ.get("SECRET_TEST_A")
-b = os.environ.get("SECRET_TEST_B")
-
-with open(os.path.join(os.getcwd(), 'testing.txt'), 'w') as f:
-    f.write(a)
-    f.write('\n')
-    f.write(b)

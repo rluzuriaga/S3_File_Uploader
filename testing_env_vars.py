0da9494a051d6da8566b9cd7eac706fd89e13fa5
rluzuriaga <rodrigoluzuriaga@icloud.com>
2021-02-07 19:47:24 -0800

    More testing

--- testing_env_vars.py
+++ testing_env_vars.py
@@ -3,5 +3,7 @@ import os
 a = os.environ.get("SECRET_TEST_A")
 b = os.environ.get("SECRET_TEST_B")
 
-print(a.split())
-print(b.split())
+with open(os.path.join(os.getcwd(), 'testing.txt')) as f:
+    f.write(a)
+    f.write('\n')
+    f.write(b)

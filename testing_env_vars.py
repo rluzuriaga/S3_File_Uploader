980709b25592285b83bb22de1ca7bc6d0a6bdf17
rluzuriaga <rodrigoluzuriaga@icloud.com>
2021-02-07 19:39:06 -0800

    More Actions tests

--- testing_env_vars.py
+++ testing_env_vars.py
@@ -1,4 +1,7 @@
 import os
 
-print(os.environ.get("SECRET_TEST_A"))
-print(os.environ.get("SECRET_TEST_B"))
+a = os.environ.get("SECRET_TEST_A")
+b = os.environ.get("SECRET_TEST_B")
+
+print(a.split())
+print(b.split())
